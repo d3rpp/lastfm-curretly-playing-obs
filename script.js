08a576288028d3025f95be02abfc786e80dcdb86
Hudson Curren <hudson.curren@icloud.com>
2021-12-26 18:55:01 +1300

    added some padding, removed the SVG being inserted again every update moved the footer further in (easier to read)

--- script.js
+++ script.js
@@ -17,11 +17,6 @@ if (LASTFM_API_KEY == '' || LASTFM_USER == '') {
 // API URL
 const API_URL = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=${LASTFM_USER}&format=json&api_key=${LASTFM_API_KEY}`;
 
-// the icons, svgs wrapped in divs
-const SONG_ICON = `<div class="img"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#eeeeee"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 3l.01 10.55c-.59-.34-1.27-.55-2-.55C7.79 13 6 14.79 6 17s1.79 4 4.01 4S14 19.21 14 17V7h4V3h-6zm-1.99 16c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg></div>`;
-const ALBUM_ICON = `<div class="img"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#eeeeee"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-12.5c-2.49 0-4.5 2.01-4.5 4.5s2.01 4.5 4.5 4.5 4.5-2.01 4.5-4.5-2.01-4.5-4.5-4.5zm0 5.5c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"/></svg></div>`;
-const ARTIST_ICON = `<div class="img"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#eeeeee"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg></div>`;
-
 // Update Interval
 // do not set below 1
 // number for seconds to update it
@@ -43,13 +38,13 @@ const LOADING = document.querySelector('.load-container');
 const INFO = document.querySelector('#info');
 
 /** @type HTMLElement */
-const NAME = document.querySelector('#info .song');
+const NAME = document.querySelector('#info .song span');
 
 /** @type HTMLElement */
-const ALBUM = document.querySelector('#info .album');
+const ALBUM = document.querySelector('#info .album span');
 
 /** @type HTMLElement */
-const ARTIST = document.querySelector('#info .artist');
+const ARTIST = document.querySelector('#info .artist span');
 
 /** @type HTMLElement */
 const IP = document.querySelector('#info footer');
@@ -77,17 +72,17 @@ if (!ARTWORK || !LOADING || !NAME || !ALBUM || !ARTIST || !INFO || !IP) {
  * @param {string} padding
  * @returns void
  */
-const updateValueIfChanged = (el, val, maxLength, padding) => {
-	if (el.textContent.includes(val)) return;
-
+const updateValueIfChanged = (el, val, maxLength) => {
 	let tmp = val;
 
 	if (tmp.length > maxLength) {
 		tmp = `${tmp.substring(0, maxLength)}...`;
 	}
 
-	el.setAttribute('style', "opacity: 0; transform: translateX('8px');");
-	el.innerHTML = `${padding} ${DOMPurify.sanitize(tmp)}`;
+	if (el.textContent.includes(tmp)) return;
+
+	el.setAttribute('style', 'opacity: 0;');
+	el.innerText = `${DOMPurify.sanitize(tmp)}`;
 
 	el.removeAttribute('style');
 };
@@ -141,39 +136,26 @@ const update = async () => {
 
 	// checks if value has updated
 
-	updateValueIfChanged(NAME, song, 25, SONG_ICON);
-	updateValueIfChanged(ALBUM, album, 35, ALBUM_ICON);
-	updateValueIfChanged(ARTIST, artist, 35, ARTIST_ICON);
+	updateValueIfChanged(NAME, song, 25);
+	updateValueIfChanged(ALBUM, album, 35);
+	updateValueIfChanged(ARTIST, artist, 35);
 
 	// it is no longer empty, only effective on initial load
 	INFO.classList.toggle('empty', false);
 	IP.classList.toggle('hidden', false);
 
+	const WIDTH = getMax([
+		NAME.getBoundingClientRect().width + 48,
+		ALBUM.getBoundingClientRect().width + 36,
+		ARTIST.getBoundingClientRect().width + 36,
+		300,
+	]);
+
 	// set the style of the info to the necessary width, allows for CSS animations
-	INFO.setAttribute(
-		'style',
-		`width: ${getMax([
-			NAME.getBoundingClientRect().width,
-			ALBUM.getBoundingClientRect().width,
-			ARTIST.getBoundingClientRect().width,
-			300,
-		])}px;`
-	);
+	INFO.setAttribute('style', `width: ${WIDTH}px;`);
 
 	// moves the required "powered by AudioScrobbler" to keep it on the right of the content
-	IP.setAttribute(
-		'style',
-		`transform: translateX(${
-			getMax([
-				NAME.getBoundingClientRect().width,
-				ALBUM.getBoundingClientRect().width,
-				ARTIST.getBoundingClientRect().width,
-				300,
-			]) -
-			300 +
-			48
-		}px);`
-	);
+	IP.setAttribute('style', `transform: translateX(${WIDTH - 236}px);`);
 };
 
 // get the maximum of a bunch of numbers, used to set the width of the info panel to the max size of the text
