a15827788ca0d3cf9dfa842ddf0a0822e89b6a47
Hudson Curren d3rpp <hudson.curren@icloud.com>
2022-03-10 10:42:23 +1300

    updated footer

--- script.js
+++ script.js
@@ -132,6 +132,7 @@ const update = async () => {
 	if (DISAPPEAR_ON_NOT_PLAYING) {
 		BODY.classList.toggle('current', is_current);
 		INFO.classList.toggle('current', is_current);
+		IP.setAttribute('style', `transform: translateX(-236px);opacity:0;`);
 	}
 
 	if (!is_current && DISAPPEAR_ON_NOT_PLAYING) return;
